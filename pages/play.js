fdd57a8bfa42408a6dbb67e497cda5d1b0902a73
JaydenMoore <jaydenmoore456@gmail.com>
2023-05-17 00:35:07 -0400

    modified:   pages/play.js

--- pages/play.js
+++ pages/play.js
@@ -133,7 +133,7 @@ function Home() {
             placeholder="guess here"
             className="focus:ring-1 focus:ring-black focus:outline-none w-[16.9rem] p-2 border-2 rounded-md border-slate-100"
             />
-          <button className="bg-white focus:ring-1 focus:ring-black focus:outline-none w-[4.5rem] h-[2.6rem] p-2 border-2 rounded-md border-slate-100 ml-[0.5rem]" onClick={()=>{
+          <button className="bg-white focus:ring-1 focus:ring-black focus:outline-none w-[4.5rem] h-[2.72rem] p-2 border-2 rounded-md border-slate-100 ml-[0.5rem]" onClick={()=>{
               <p>guess</p>
             }}>
             </button>
