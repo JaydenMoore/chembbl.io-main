729ebd8300c735669ec13e48af79bb4bbd878cb4
JaydenMoore <jaydenmoore456@gmail.com>
2023-05-18 21:48:42 -0400

    Co-authored-by: kinhas06 <kinhas06@users.noreply.github.com>

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,24 +1,65 @@
 import { Server } from 'socket.io'
+var lobby = []
+var currentPlayer = 0
+var globalSocket
+
+function beginRound() {
+  globalSocket.broadcast.emit("start-round", lobby[currentPlayer])
+  globalSocket.broadcast.emit("begin-timer", 60)
+
+  var counter = 0;
+  var timer = 60
+  var i = setInterval(function(){
+    timer--;
+    globalSocket.broadcast.emit("update-timer", timer)
+      counter++;
+      if(counter === 60) {
+        clearInterval(i);
+        currentPlayer++;
+        beginRound();     
+      }
+  }, 200);
+}
 
 const SocketHandler = (req, res) => {
-  var lobby = []
+  var chatLog = []
+  var timer = 30
+  var waitTimerStarted = false
   if (res.socket.server.io) {
-    console.log('Socket is already running')
+    //console.log('Socket is already running')
   } else {
-    console.log('Socket is initializing')
+    //console.log('Socket is initializing')
     const io = new Server(res.socket.server)
     res.socket.server.io = io
 
     io.on('connection', socket => {
+        globalSocket = socket
         socket.on('new-player-joined', player => {
           lobby.push(player)
           socket.broadcast.emit('add-new-player', lobby)
+          
+          if (lobby.length >= 2 && !waitTimerStarted) {
+            socket.broadcast.emit("begin-timer", 30)
+            timer = 30
+            waitTimerStarted = true
+            var counter = 0;
+            var i = setInterval(function(){
+              timer--;
+              socket.broadcast.emit("update-timer", timer)
+                counter++;
+                if(counter === 30) {
+                    clearInterval(i);
+                    beginRound(socket);
+                }
+            }, 100);
+          }
+        })
+        socket.on('new-msg', msg => {
+          chatLog.push(msg)
+          socket.broadcast.emit('update-chat-log', chatLog)
         })
         socket.on('canvas-change', canvas => {
           socket.broadcast.emit('update-canvas', canvas)
-        })
-        socket.on('confirm-lobby', lobby => {
-          socket.broadcast.emit('double-update-lobby', lobby)
         })
     })
   }
